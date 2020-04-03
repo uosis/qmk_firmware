762a2f89342ec19ff067c81453c4a459ad541f74
uosis <uosisl@gmail.com>
2020-04-02 19:13:43 -0600

    move ergodox config in

---
+++ keyboards/ergodox_ez/keymaps/uosis/ergodox_ez_uosis-qwerty_XbLaB_with-print/src/keymap.c
@@ -0,0 +1,91 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#include "keymap_german.h"
+#include "keymap_nordic.h"
+#include "keymap_french.h"
+#include "keymap_spanish.h"
+#include "keymap_hungarian.h"
+#include "keymap_swedish.h"
+#include "keymap_br_abnt2.h"
+#include "keymap_canadian_multilingual.h"
+#include "keymap_german_ch.h"
+
+#include <print.h>
+
+#define KC_MAC_UNDO LGUI(KC_Z)
+#define KC_MAC_CUT LGUI(KC_X)
+#define KC_MAC_COPY LGUI(KC_C)
+#define KC_MAC_PASTE LGUI(KC_V)
+#define KC_PC_UNDO LCTL(KC_Z)
+#define KC_PC_CUT LCTL(KC_X)
+#define KC_PC_COPY LCTL(KC_C)
+#define KC_PC_PASTE LCTL(KC_V)
+#define NO_TH ALGR(KC_T)
+#define NO_ETH ALGR(KC_D)
+
+enum custom_keycodes {
+  RGB_SLD = SAFE_RANGE, // can always be here
+  TOGGLE_LAYER_COLOR,
+  EPRM,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_ergodox(KC_GRAVE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,LGUI_T(KC_BSLASH),KC_Q,KC_W,KC_E,KC_R,KC_T,KC_LBRACKET,LCTL_T(KC_ESCAPE),KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSPO,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_LCBR,TT(2),KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_DELETE,KC_WWW_BACK,KC_HOME,KC_SPACE,KC_BSPACE,KC_END,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_EQUAL,KC_QUOTE,KC_RBRACKET,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_DQUO,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,RCTL_T(KC_ESCAPE),KC_RCBR,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,KC_KP_7,KC_KP_4,KC_KP_6,KC_KP_9,OSL(1),KC_LALT,LCTL(KC_TAB),KC_PGUP,KC_PGDOWN,KC_TAB,KC_ENTER),
+
+  [1] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_DOT,KC_TRANSPARENT,LSFT(KC_KP_SLASH),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LALT,KC_KP_SLASH,KC_KP_ASTERISK,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_ENTER,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LSHIFT,KC_LCTRL,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_PLUS,KC_TRANSPARENT,KC_KP_7,KC_KP_8,KC_KP_9,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_4,KC_KP_5,KC_KP_6,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_MINUS,KC_KP_0,KC_KP_1,KC_KP_2,KC_KP_3,KC_TRANSPARENT,KC_RALT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TAB,KC_TRANSPARENT),
+
+  [2] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LCTL_T(KC_ESCAPE),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LSPO,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(0),KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_DELETE,KC_TRANSPARENT,KC_HOME,KC_SPACE,KC_BSPACE,KC_END,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,RCTL_T(KC_ESCAPE),KC_TRANSPARENT,KC_F11,KC_F12,KC_F13,KC_F14,KC_F15,KC_RSPC,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LALT,KC_TRANSPARENT,KC_PGUP,KC_PGDOWN,KC_TAB,KC_ENTER),
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  uprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+  
+  switch (keycode) {
+    case EPRM:
+      if (record->event.pressed) {
+        eeconfig_init();
+      }
+      return false;
+  }
+  return true;
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+  uint8_t layer = biton32(state);
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+  switch (layer) {
+    case 1:
+      ergodox_right_led_1_on();
+      break;
+    case 2:
+      ergodox_right_led_2_on();
+      break;
+    case 3:
+      ergodox_right_led_3_on();
+      break;
+    case 4:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      break;
+    case 5:
+      ergodox_right_led_1_on();
+      ergodox_right_led_3_on();
+      break;
+    case 6:
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      break;
+    case 7:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      break;
+    default:
+      break;
+  }
+  return state;
+};
---
+++ keyboards/ergodox_ez/keymaps/uosis/flash.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+teensy_loader_cli -v -w --mcu atmega32u4 *.hex
