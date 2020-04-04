aee666b2f5de379946b68438f29b8502234f8a10
uosis <uosisl@gmail.com>
2020-04-04 12:28:04 -0600

    make log message easier to parse

--- keyboards/ergodox_ez/keymaps/uosis/keymap.c
+++ keyboards/ergodox_ez/keymaps/uosis/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  uprintf("K: kc: %u, col: %u, row: %u, pr: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+  uprintf("K: kc=%u col=%u row=%u pr=%u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
   
   switch (keycode) {
     case EPRM:
